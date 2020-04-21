b23bf8d2539e3f9e80b0614d6c1546cf0f77f37d
Stephanie Bauer <stephanie.bauer@telekom.de>
2020-04-21 16:59:52 +0200

    finished clip 14

---
+++ app/src/main/java/com/example/dogsapp/view/ListFragment.java
@@ -0,0 +1,24 @@
+package com.example.dogsapp.view;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dogsapp.R;
+
+public class ListFragment extends Fragment {
+
+    public ListFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+}
--- app/src/main/java/com/example/dogsapp/view/MainActivity.java
+++ app/src/main/java/com/example/dogsapp/view/MainActivity.java
@@ -1,10 +1,13 @@
-package com.example.dogsapp;
+package com.example.dogsapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.dogsapp.R;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
