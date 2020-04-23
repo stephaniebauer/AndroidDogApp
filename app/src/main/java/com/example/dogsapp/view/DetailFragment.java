93736f692fecd97deef45f2f52703d0babc06708
Stephanie Bauer <stephanie.bauer@telekom.de>
2020-04-23 14:42:38 +0200

    finished part 19

--- app/src/main/java/com/example/dogsapp/view/DetailFragment.java
+++ app/src/main/java/com/example/dogsapp/view/DetailFragment.java
@@ -2,15 +2,26 @@ package com.example.dogsapp.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.dogsapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DetailFragment extends Fragment {
+
+    private int dogUuid;
 
     public DetailFragment() {
     }
@@ -19,6 +30,17 @@ public class DetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if(getArguments() != null){
+            dogUuid = DetailFragmentArgs.fromBundle(getArguments()).getDogUuid();
+        }
+    }
+
 }
